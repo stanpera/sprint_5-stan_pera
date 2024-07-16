186b36a0ab69be68e7d0ccba832fa83a767557cb
stanpera <stanislawpera94@gmail.com>
2024-07-16 12:19:03 +0200

    Aktualizacja kodu sprint_5

--- sprint5.js
+++ sprint5.js
@@ -26,48 +26,55 @@ const users = [
   },
 ];
 
-// ZADANIE 1 
-const nickGenerator = users
-  .filter(
-    (user) =>
-      typeof user.firstName === "string" &&
-      user.firstName.length > 2 &&
-      typeof user.lastName === "string" &&
-      user.lastName.length > 2
-  )
-  .map((user) => {
-    const firstNamePart = user.firstName.slice(-3).split("").reverse().join("");
-    const lastNamePart = user.lastName.slice(0, 3).split("").reverse().join("");
+// ZADANIE 1
+const nickGenerator = users.map((user) => {
+  const { firstName, lastName } = user;
+  if (
+    typeof firstName === "string" &&
+    firstName.length > 2 &&
+    typeof lastName === "string" &&
+    lastName.length > 2
+  ) {
+    const firstNamePart = firstName.slice(-3).split("").reverse().join("");
+    const lastNamePart = lastName.slice(0, 3).split("").reverse().join("");
     const nickname =
-      (firstNamePart + lastNamePart).charAt(0).toUpperCase() +
-      (firstNamePart + lastNamePart).slice(1).toLowerCase();
-
+      `${firstNamePart}${lastNamePart}`.charAt(0).toUpperCase() +
+      `${firstNamePart}${lastNamePart}`.slice(1).toLowerCase();
     return { ...user, nickname };
-  });
-
+  } else {
+    return { ...user };
+  }
+});
 // console.log(nickGenerator);
 
-// ZADANIE 2 
-const ageGenerator = nickGenerator.map((user, index) => {
-  const firstNameLength = user.firstName.length;
-  const lastNameLength = user.lastName.length;
-  const firstAndLastNameLength = firstNameLength + lastNameLength;
-  let age = 0;
-  if (firstAndLastNameLength % 2 === 0) {
-    age += firstAndLastNameLength;
-  } else {
-    const dynamicKeys = ["firstName", "lastName", "nickname"];
-    const dynamicSum = dynamicKeys.reduce((sum, key) => sum + key.length, 0);
-    age = Math.ceil(dynamicSum / (index === 0 ? 1 : index));
-  }
-  return { ...user, age };
-});
+// ZADANIE 2
+const ageGenerator = nickGenerator
+  .filter((user) => {
+    const { firstName, lastName, nickname } = user;
+    return firstName, lastName, nickname;
+  })
+  .map((user, index) => {
+    const { firstName, lastName} = user;
+    const firstNameLength = firstName.length;
+    const lastNameLength = lastName.length;
+    const firstAndLastNameLength = firstNameLength + lastNameLength;
+    let age = 0;
+    if (firstAndLastNameLength % 2 === 0) {
+      age += firstAndLastNameLength;
+    } else {
+      const dynamicKeys = Object.keys({firstName, lastName, nickname});
+      const dynamicSum = dynamicKeys.reduce((sum, key) => sum + key.length, 0);
+      age = Math.ceil(dynamicSum / (index === 0 ? 1 : index));
+    }
+    return { ...user, age };
+  });
 // console.log(ageGenerator);
 
-// ZADANIE 3 
+// ZADANIE 3
 const mostCommonletterGenerator = ageGenerator.map((user) => {
+  const { firstName, lastName, nickname } = user;
   const stringObject =
-    `${user.firstName}${user.lastName}${user.nickname}`.toLowerCase();
+    `${firstName}${lastName}${nickname}`.toLowerCase();
   const counter = stringObject.split("").reduce((sumOfLetters, letter) => {
     if (/[a-z]/.test(letter)) {
       sumOfLetters[letter] = (sumOfLetters[letter] || 0) + 1;
@@ -75,16 +82,19 @@ const mostCommonletterGenerator = ageGenerator.map((user) => {
     return sumOfLetters;
   }, {});
   let mostCommonLetter = null;
-  let maxScore = 0;
+  let maxCount = 0;
 
-  for (const [letter, score] of Object.entries(counter)) {
-    if (score > maxScore || (score === maxScore && letter < mostCommonLetter)) {
-      maxScore = score;
+  for (const [letter, count] of Object.entries(counter)) {
+    if (count > maxCount || (count === maxCount && letter < mostCommonLetter)) {
+      maxCount = count;
       mostCommonLetter = letter;
     }
   }
-  let topLetterwithNumber = `top letter: ${mostCommonLetter}, amount: ${maxScore}`;
-  return { ...user, topLetterwithNumber };
+
+  return {
+    ...user,
+    mostCommonLetter: { letter: mostCommonLetter, count: maxCount },
+  };
 });
 
 console.log(mostCommonletterGenerator);
